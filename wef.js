0f51f196ce64ec454950d108dec380d55ae78e0c
Sam Trapskin <samtrapskin@gmail.com>
2018-06-13 22:49:01 -0500

    more tweaks css

--- wef.js
+++ wef.js
@@ -2,15 +2,16 @@
 <div class="container">
     <div class="row">
         <div class="col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-0">
-            <!-- START SECTION HEADING -->
+            {/* <!-- START SECTION HEADING --> */}
             <div class="section-heading section-heading--left m-b-sm-60 m-b-xxs-30 m-b-md-0">
                 <h2 class="wow fadeInLeftSmall">My Skills</h2>
                 <p class="wow fadeInLeftSmall" data-wow-delay=".2s">
                     I’m John Doe. I designed this site so you can get to know me on a personal level. My career as a UX Designer, my portfolio, my personal photography, my blog, my favorite quotes, and more.
                 </p>
             </div>
-            <!-- END SECTION HEADING -->
-        </div><!-- /.col-sm-12 col-md-4 -->
+            {/* <!-- END SECTION HEADING --> */}
+        </div>
+        {/* <!-- /.col-sm-12 col-md-4 --> */}
         <div class="col-sm-12 col-md-8">
             <div class="skill-slider owl-carousel" data-owl-options='{"margin":20,"loop":false,"smartSpeed":900,"dots":false,"nav":true,"navSpeed":900,"responsiveClass":true,"responsive":{"0":{"items":1},"480":{"items":2},"768":{"items":3},"992":{"items":2},"1200":{"items":3}}}'>
                 <div class="single-skill text-center wow fadeInUp">
@@ -113,97 +114,14 @@
                     </div>
                 </div>
             </div>
-        </div><!-- /.col-sm-12 col-md-8 -->
-    </div><!-- /.row -->
-</div><!-- /.container -->
-</section>
-<!-- END SKILLS -->
+        </div>
+        {/* <!-- /.col-sm-12 col-md-8 --> */}
+    </div>
+    {/* <!-- /.row --> */}
 </div>
-<!-- END ABOUT -->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!-- START SKILLS
-========================================================================== -->
-<section class="skills" class="section-padding section-main section">
-    <div class="container">
-        
-                <!-- START SECTION HEADING -->
-                    <h2 class="wow fadeInLeftSmall">My Skills</h2>
-                        Here balls a list of programming languages that I have experience working with. 
-                    </p>
-                    <br/>
-                    <br/>
-
-                    <ul>
-                        <li class="react_icon"><img src="https://i.imgur.com/LNeZgkh.png" height="105" width="110"></li>
-
-                        <li class="icons"><img src="https://i.imgur.com/TmhYez5.png" height="55" width="185"></li>
-                        <li id="node"><img src="https://i.imgur.com/1qtvquR.png" height="75" width="150"></li>
-                        <li id="sql"><img src="https://i.imgur.com/ADKrKDU.png" height="109" width="125"></li>                        
-                        <li id="express"><img src="https://i.imgur.com/RZOi1G7.png" height="25" width="120"></li>        
-
-                    </ul>
-                        <br />
-                        <br />
-
-                    <ul>
-                        <li id="adobe"><img src= "https://i.imgur.com/6TcUIUv.png" height="85" width="170"></li>
-                        <li id="html"><img src= "https://i.imgur.com/FBUtxUt.png" height="65" width="75"></li>
-                        <li id="css"><img src="https://i.imgur.com/sr521ER.png" height="75" width="85"></li>
-                        <li id="GitHub"><img src="https://i.imgur.com/BMrtB9a.png" height="25" width="90"></li>
-                        <li id="GitHub-Cat"><img src="https://i.imgur.com/Y1brT1c.png" height="25" width="30"></li>
-                        <li id="material_ui"><img src="https://i.imgur.com/aium4M5.png" height="95" width="98"></li>
-                        <li id="bootstrap"><img src= "https://i.imgur.com/nzizXRY.png" height="75" width="91"></li>
-                    </ul>
-                        </div>
-                <!-- END SECTION HEADING -->
-           
-             
-    </div><!-- /.container -->
+{/* <!-- /.container --> */}
 </section>
-<!-- END SKILLS -
+// <!-- END SKILLS -->
+
+
+// <!-- END ABOUT -->
