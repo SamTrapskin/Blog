8c57c0d3f5b7cc96257d0daa0c6716e0191454ce
Sam Trapskin <samtrapskin@gmail.com>
2018-06-27 13:46:31 -0500

    added css

---
+++ .history/samtrapskin.github.io/wef_20180627133446.js
@@ -0,0 +1,210 @@
+<section class="skills section-padding">
+<div class="container">
+    <div class="row">
+        <div class="col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-0">
+            {/* <!-- START SECTION HEADING --> */}
+            <div class="section-heading section-heading--left m-b-sm-60 m-b-xxs-30 m-b-md-0">
+                <h2 class="wow fadeInLeftSmall">My Skills</h2>
+                <p class="wow fadeInLeftSmall" data-wow-delay=".2s">
+                    I’m John Doe. I designed this site so you can get to know me on a personal level. My career as a UX Designer, my portfolio, my personal photography, my blog, my favorite quotes, and more.
+                </p>
+            </div>
+            {/* <!-- END SECTION HEADING --> */}
+        </div><!-- /.col-sm-12 col-md-4 -->
+        <div class="col-sm-12 col-md-8">
+            <div class="skill-slider owl-carousel" data-owl-options='{"margin":20,"loop":false,"smartSpeed":900,"dots":false,"nav":true,"navSpeed":900,"responsiveClass":true,"responsive":{"0":{"items":1},"480":{"items":2},"768":{"items":3},"992":{"items":2},"1200":{"items":3}}}'>
+                <div class="single-skill text-center wow fadeInUp">
+                    <div class="single-skill__v-center">
+                        <i class="zmdi zmdi-drink"></i>
+                        <div class="progress-bar">
+                            <h4 class="progress-bar__label">HTML - <span>80%</span></h4>
+                            <div class="progress-bar__bar">
+                                <div class="progress-bar__bar__active"></div>
+                            </div>
+                        </div>                                    
+                    </div>
+                </div>
+                <div class="single-skill text-center wow fadeInUp" data-wow-delay=".1s">
+                    <div class="single-skill__v-center">
+                        <i class="zmdi zmdi-mouse"></i>
+                        <div class="progress-bar">
+                            <h4 class="progress-bar__label">Photoshop - <span>90%</span></h4>
+                            <div class="progress-bar__bar">
+                                <div class="progress-bar__bar__active"></div>
+                            </div>
+                        </div>                                    
+                    </div>
+                </div>
+                <div class="single-skill text-center wow fadeInUp" data-wow-delay=".2s">
+                    <div class="single-skill__v-center">
+                        <i class="zmdi zmdi-boat"></i>
+                        <div class="progress-bar">
+                            <h4 class="progress-bar__label">Illustrator - <span>85%</span></h4>
+                            <div class="progress-bar__bar">
+                                <div class="progress-bar__bar__active"></div>
+                            </div>
+                        </div>                                    
+                    </div>
+                </div>
+                <div class="single-skill text-center wow fadeInUp" data-wow-delay=".3s">
+                    <div class="single-skill__v-center">
+                        <i class="zmdi zmdi-drink"></i>
+                        <div class="progress-bar">
+                            <h4 class="progress-bar__label">Design - <span>90%</span></h4>
+                            <div class="progress-bar__bar">
+                                <div class="progress-bar__bar__active"></div>
+                            </div>
+                        </div>                                    
+                    </div>
+                </div>
+                <div class="single-skill text-center wow fadeInUp" data-wow-delay=".4s">
+                    <div class="single-skill__v-center">
+                        <i class="zmdi zmdi-mouse"></i>
+                        <div class="progress-bar">
+                            <h4 class="progress-bar__label">JavaScript - <span>85%</span></h4>
+                            <div class="progress-bar__bar">
+                                <div class="progress-bar__bar__active"></div>
+                            </div>
+                        </div>                                    
+                    </div>
+                </div>
+                <div class="single-skill text-center wow fadeInUp" data-wow-delay=".5s">
+                    <div class="single-skill__v-center">
+                        <i class="zmdi zmdi-boat"></i>
+                        <div class="progress-bar">
+                            <h4 class="progress-bar__label">jQuery - <span>90%</span></h4>
+                            <div class="progress-bar__bar">
+                                <div class="progress-bar__bar__active"></div>
+                            </div>
+                        </div>                                    
+                    </div>
+                </div>
+                <div class="single-skill text-center wow fadeInUp" data-wow-delay=".6s">
+                    <div class="single-skill__v-center">
+                        <i class="zmdi zmdi-drink"></i>
+                        <div class="progress-bar">
+                            <h4 class="progress-bar__label">PHP - <span>85%</span></h4>
+                            <div class="progress-bar__bar">
+                                <div class="progress-bar__bar__active"></div>
+                            </div>
+                        </div>                                    
+                    </div>
+                </div>
+                <div class="single-skill text-center wow fadeInUp" data-wow-delay=".7s">
+                    <div class="single-skill__v-center">
+                        <i class="zmdi zmdi-mouse"></i>
+                        <div class="progress-bar">
+                            <h4 class="progress-bar__label">CSS - <span>95%</span></h4>
+                            <div class="progress-bar__bar">
+                                <div class="progress-bar__bar__active"></div>
+                            </div>
+                        </div>                                    
+                    </div>
+                </div>
+                <div class="single-skill text-center wow fadeInUp" data-wow-delay=".8s">
+                    <div class="single-skill__v-center">
+                        <i class="zmdi zmdi-boat"></i>
+                        <div class="progress-bar">
+                            <h4 class="progress-bar__label">SASS - <span>90%</span></h4>
+                            <div class="progress-bar__bar">
+                                <div class="progress-bar__bar__active"></div>
+                            </div>
+                        </div>                                    
+                    </div>
+                </div>
+            </div>
+        </div><!-- /.col-sm-12 col-md-8 -->
+    </div><!-- /.row -->
+</div>
+{/* <!-- /.container --> */}
+</section>
+// <!-- END SKILLS -->
+</div>
+<!-- END ABOUT -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- START SKILLS
+========================================================================== -->
+<section class="skills" class="section-padding section-main section">
+    <div class="container">
+        
+                <!-- START SECTION HEADING -->
+                    <h2 class="wow fadeInLeftSmall">My Skills</h2>
+                        Here balls a list of programming languages that I have experience working with. 
+                    </p>
+                    <br/>
+                    <br/>
+
+                    <ul>
+                        <li class="react_icon"><img src="https://i.imgur.com/LNeZgkh.png" height="105" width="110"></li>
+
+                        <li class="icons"><img src="https://i.imgur.com/TmhYez5.png" height="55" width="185"></li>
+                        <li id="node"><img src="https://i.imgur.com/1qtvquR.png" height="75" width="150"></li>
+                        <li id="sql"><img src="https://i.imgur.com/ADKrKDU.png" height="109" width="125"></li>                        
+                        <li id="express"><img src="https://i.imgur.com/RZOi1G7.png" height="25" width="120"></li>        
+
+                    </ul>
+                        <br />
+                        <br />
+
+                    <ul>
+                        <li id="adobe"><img src= "https://i.imgur.com/6TcUIUv.png" height="85" width="170"></li>
+                        <li id="html"><img src= "https://i.imgur.com/FBUtxUt.png" height="65" width="75"></li>
+                        <li id="css"><img src="https://i.imgur.com/sr521ER.png" height="75" width="85"></li>
+                        <li id="GitHub"><img src="https://i.imgur.com/BMrtB9a.png" height="25" width="90"></li>
+                        <li id="GitHub-Cat"><img src="https://i.imgur.com/Y1brT1c.png" height="25" width="30"></li>
+                        <li id="material_ui"><img src="https://i.imgur.com/aium4M5.png" height="95" width="98"></li>
+                        <li id="bootstrap"><img src= "https://i.imgur.com/nzizXRY.png" height="75" width="91"></li>
+                    </ul>
+                        </div>
+                <!-- END SECTION HEADING -->
+           
+             
+    </div><!-- /.container -->
+</section>
+<!-- END SKILLS -
